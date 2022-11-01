719cf262487b52446210764937f1b261e21c4046
Vishesh Pandey <116044800+visheshpandey2001@users.noreply.github.com>
2022-11-01 16:59:29 +0000

    added operations - addition, subtraction, and square

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matrixOperations
 
 from help import inputRules
 
-# options 
+# options
 
 # 1 - Addition of matrix
 # 2 - Subtraction of matrix
@@ -14,95 +14,135 @@ from help import inputRules
 
 print("Linear Algebra")
 
-while True :
+while True:
     print()
     print("Operations available : ")
     print("-------------------------------")
     print("1 -> Addition of matrix")
+    print()
     print("2 -> Subtraction of matrix")
+    print()
     print("3 -> Multiplication of matrix")
+    print()
     print("4 -> Square of matrix")
+    print()
     print("5 -> Determinant of matrix")
-    print("6 -> Help ( How to use ? )")
+    print()
+    print("6 -> Know input rules")
+    print()
     print("7 -> Settings ( Comming Soon ) ")
+    print()
     print("8 -> Exit program")
     print("-------------------------------")
     mainMenuChoice = input("Enter your choice : ")
 
-    if mainMenuChoice == "1" :
-        pass
-
-    elif mainMenuChoice == "2" :
-        pass
-
-    elif mainMenuChoice == "3" :
+    if mainMenuChoice == "1":
         print("Please select the order of matrix")
         print("1 -> 2x2")
         print("2 -> 3x3")
-        
+
         orderOfMatrixChoice = input("Enter youir choice :")
-        
-        if orderOfMatrixChoice == "1" :
-            
+
+        if orderOfMatrixChoice == "1":
+
+            firstOrderTwoMatrix = inputMatrix.orderTwo()
+            secondOrderTwoMatrix = inputMatrix.orderTwo()
+            res = matrixOperations.addition(firstOrderTwoMatrix,
+                                            secondOrderTwoMatrix)
+
+            print(res)
+
+        elif orderOfMatrixChoice == "2":
+            orderThreeMatrix = inputMatrix.orderThree()
+
+    elif mainMenuChoice == "2":
+        pass
+
+    elif mainMenuChoice == "3":
+        print("Please select the order of matrix")
+        print("1 -> 2x2")
+        print("2 -> 3x3")
+
+        orderOfMatrixChoice = input("Enter youir choice :")
+
+        if orderOfMatrixChoice == "1":
+
             firstOrderTwoMatrix = inputMatrix.orderTwo()
 
             secondOrderTwoMatrix = inputMatrix.orderTwo()
 
-            res = matrixOperations.multiply(firstOrderTwoMatrix , secondOrderTwoMatrix)
+            res = matrixOperations.multiply(firstOrderTwoMatrix,
+                                            secondOrderTwoMatrix)
 
-            print(res)
-            
-            
+            for i in res:
+                print(i)
 
-        elif orderOfMatrixChoice == "2" :
+        elif orderOfMatrixChoice == "2":
             orderThreeMatrix = inputMatrix.orderThree()
 
         else:
             print("Invalid input")
-        
-       
 
+    elif mainMenuChoice == "4":
+        print("Please select the order of matrix")
+        print("1 -> 2x2")
+        print("2 -> 3x3")
 
-    elif mainMenuChoice == "4" :
-        pass
+        orderOfMatrixChoice = input("Enter youir choice :")
+
+        if orderOfMatrixChoice == "1":
+
+            firstOrderTwoMatrix = inputMatrix.orderTwo()
+
+            
+
+            res = matrixOperations.square(firstOrderTwoMatrix)
+
+            print(res)
+
+        elif orderOfMatrixChoice == "2":
+            orderThreeMatrix = inputMatrix.orderThree()
+
+        else:
+            print("Invalid input")
 
     # Finding determinant of matrix
-    elif mainMenuChoice == "5" :
+    elif mainMenuChoice == "5":
         print("Please select the order of matrix")
         print("1 -> 2x2")
         print("2 -> 3x3")
         orderOfMatrixChoice = input("Enter youir choice :")
-        
-        if orderOfMatrixChoice == "1" :
+
+        if orderOfMatrixChoice == "1":
             orderTwoMatrix = inputMatrix.orderTwo()
             det = determinant.orderTwo(orderTwoMatrix)
             print("Determinant of given matrix is :")
             print(det)
 
-        elif orderOfMatrixChoice == "2" :
+        elif orderOfMatrixChoice == "2":
             orderThreeMatrix = inputMatrix.orderThree()
             det = determinant.orderThree(orderThreeMatrix)
             print("Determinant of give matrix is : ")
             print(det)
 
-        else :
+        else:
             print("Invalid input ( Only order 2x2 and 3x3 is allowed )")
 
-    elif mainMenuChoice == "6" :
+    elif mainMenuChoice == "6":
         print("1 - Input Rules")
         print("2 - Go back to Main Menu")
 
         helpChoice = input("Enter your choice :")
 
-        if helpChoice == 1 :
+        if helpChoice == "1":
             inputRules()
 
-    elif mainMenuChoice == "7" :
+    elif mainMenuChoice == "7":
         print("Settings - Comming Soon")
-    
-    elif mainMenuChoice == "8" :
+
+    elif mainMenuChoice == "8":
         print("Thank you")
         break
-    
+
     else:
         print("Invalid input. Please try again")
--- matrixOperations.py
+++ matrixOperations.py
@@ -1,8 +1,42 @@
-def addition(x,y):
-    pass
+def addition(x,y, res=[]):
+    try:
+    
+        for i in x :
+          res.append([])
+            
+        for i in range(len(y)):
+          for j in range(len(y[0])) :
+            res[i].append(0)
+              
+        for i in range(len(x)):           
+          for j in range(len(y[0])):
+            res[i][j] = x[i][j] + y[i][j] 
+        return res
+    
+    except Exception as e :
+        print("Error occured")
+        print("Error ::" , e)
+        return False
 
-def subtraction(x,y):
-    pass
+def subtraction(x,y, res=[]):
+    try:
+    
+        for i in x :
+          res.append([])
+            
+        for i in range(len(y)):
+          for j in range(len(y[0])) :
+            res[i].append(0)
+              
+        for i in range(len(x)):   
+            for j in range(len(y[0])):
+                res[i][j] = x[i][j] - y[i][j] 
+        return res
+    
+    except Exception as e :
+        print("Error occured")
+        print("Error ::" , e)
+        return False
 
 def multiply(x,y,res=[]):
 
@@ -25,5 +59,31 @@ def multiply(x,y,res=[]):
         print("Error occured")
         print("Error ::" , e)
         return False
+
+
+
+
+def square(x,res=[]):
+    y = x
+    try :
+        # creating res matrix with value 0
+        for i in x :
+            res.append([])
+            
+        for i in range(len(y)):
+            for j in range(len(y[0])) :
+                res[i].append(0)
+            
+        for i in range(len(x)):
+            for j in range(len(y[0])):
+                for k in range(len(y)):
+                    res[i][j] += x[i][k] * y[k][i]
+        return res  # Return the multiplication
+
+    except Exception as e :
+        print("Error occured")
+        print("Error ::" , e)
+        return False
+
 
 
